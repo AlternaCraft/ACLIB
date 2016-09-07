321dae63d8bd519a5482eef9c67f2013bc7140fa
lKingShadow <edgar.garcia12@hotmail.com>
2016-09-07 18:59:59 +0200

    Some Location utils added

---
+++ src/main/java/com/alternacraft/aclib/utils/LocationUtils.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2016 AlternaCraft
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.alternacraft.aclib.utils;
+
+import org.bukkit.block.BlockFace;
+
+/**
+ * This class contains some utils for Location objects.
+ *
+ * @author AlternaCraft
+ */
+public class LocationUtils {
+    /**
+     * Returns the facing direction from an specified yaw.
+     *
+     * @param bf
+     * @return
+     */
+    public static float blockFaceToYaw(BlockFace bf) {
+        switch (bf) {
+            case NORTH:
+                return 180f;
+            case NORTH_EAST:
+                return 135f;
+            case EAST:
+                return 90f;
+            case SOUTH_EAST:
+                return 45f;
+            case SOUTH:
+                return 360f;
+            case SOUTH_WEST:
+                return 315f;
+            case WEST:
+                return 270f;
+            case NORTH_WEST:
+                return 225f;
+            default:
+                return 0f;
+        }
+    }
+
+    /**
+     * Returns the facing direction from an specified yaw.
+     *
+     * @param yaw
+     * @return
+     */
+    public static BlockFace yawToFace(float yaw) {
+        double rotation = (yaw - 90) % 360;
+        if (rotation < 0) {
+            rotation += 360.0;
+        }
+
+        if (0 <= rotation && rotation < 22.5) {
+            return BlockFace.NORTH;
+        } else if (22.5 <= rotation && rotation < 67.5) {
+            return BlockFace.NORTH_EAST;
+        } else if (67.5 <= rotation && rotation < 112.5) {
+            return BlockFace.EAST;
+        } else if (112.5 <= rotation && rotation < 157.5) {
+            return BlockFace.SOUTH_EAST;
+        } else if (157.5 <= rotation && rotation < 202.5) {
+            return BlockFace.SOUTH;
+        } else if (202.5 <= rotation && rotation < 247.5) {
+            return BlockFace.SOUTH_WEST;
+        } else if (247.5 <= rotation && rotation < 292.5) {
+            return BlockFace.WEST;
+        } else if (292.5 <= rotation && rotation < 337.5) {
+            return BlockFace.NORTH_WEST;
+        } else if (337.5 <= rotation && rotation < 360.0) {
+            return BlockFace.NORTH;
+        } else {
+            return null;
+        }
+    }
+}
