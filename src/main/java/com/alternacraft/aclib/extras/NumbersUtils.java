4080fcae4e38dfa977655ae0f7c3210903e2e881
cube <cube@JULIAN-TABLET>
2017-08-20 17:46:57 +0200

    Fixed bug on generating randoms.

--- src/main/java/com/alternacraft/aclib/extras/NumbersUtils.java
+++ src/main/java/com/alternacraft/aclib/extras/NumbersUtils.java
@@ -16,6 +16,9 @@
  */
 package com.alternacraft.aclib.extras;
 
+import java.util.Random;
+import org.bukkit.util.Vector;
+
 /**
  * An utils class about numbers.
  *
@@ -106,4 +109,49 @@ public class NumbersUtils {
     public static boolean isBetween(float value, float lower, float bigger) {
         return value > lower && value < bigger;
     }
+    
+    /**
+     * Get a random value between an interval.
+     *
+     * @param max Max value
+     * @param min Min value
+     *
+     * @return int
+     */
+    public static int rand(int min, int max) {
+        return new Random().nextInt((max - min) + 1) + min;
+    }
+
+    /**
+     * Get a random vector contained into the two vectors.
+     *
+     * @param p1 Vector 1
+     * @param p2 Vector 2
+     *
+     * @return Random vector
+     * @since 0.0.9
+     */
+    public static Vector randBetweenVector(Vector p1, Vector p2) {
+        int pos1, pos2, pos3;
+
+        pos1 = seekMAXandGetRand(p1.getBlockX(), p2.getBlockX());
+        pos2 = seekMAXandGetRand(p1.getBlockY(), p2.getBlockY());
+        pos3 = seekMAXandGetRand(p1.getBlockZ(), p2.getBlockZ());
+
+        return new Vector(pos1, pos2, pos3);
+    }
+    
+    private static int seekMAXandGetRand(int v1, int v2) {
+        int r;
+
+        if (v1 > v2) {
+            r = rand(v2, v1);
+        } else if (v1 == v2) {
+            r = v1;
+        } else {
+            r = rand(v1, v2);
+        }
+
+        return r;
+    }    
 }
